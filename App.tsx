daaf4647fcdd3be653f95dae90eb34994ce8a124
yeonu <yeon8747@naver.com>
2024-04-15 14:52:45 +0900

    ♻️ :: react query import 수정

--- App.tsx
+++ App.tsx
@@ -2,12 +2,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParmList } from './src/types';
 import MoviesScreen from './src/screens/movie/MoviesScreen';
-import { QueryClient, QueryClientProvider } from 'react-query'
 import DetailScreen from './src/screens/detail/DetailScreen';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 
 const Stack = createNativeStackNavigator<RootStackParmList>();
-
 const queryClient = new QueryClient();
 
 const App = () => {
