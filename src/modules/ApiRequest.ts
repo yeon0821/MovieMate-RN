0d5fac4a2992a75c321cfea3a1bc69a5a1aab97a
yeonu <yeon8747@naver.com>
2024-04-15 10:13:57 +0900

    ⚡️ :: 영화 상세보기 api 추가

--- src/modules/ApiRequest.ts
+++ src/modules/ApiRequest.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { Movie } from "../types";
+import moment from "moment";
 
 const API_KEY = '37353d7bf365f487702b78b6dd8ff32b'
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
+const getImageUrl = (path: string | null) =>
+  path != null ? `${IMG_BASE_URL}/${path}` : null;
+ 
 
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
@@ -34,7 +38,11 @@ interface GetDiscoverMoviesParams {
     releaseDataLte?: string;
 }
 
-export const getDiscoverMovies =  async ({ releaseDataGte, releaseDataLte }: GetDiscoverMoviesParams) => {
+export const getDiscoverMovies =  async ({ 
+    releaseDataGte, 
+    releaseDataLte,
+    page,
+}: GetDiscoverMoviesParams) => {
 
     // 개봉 예정 영화
     const response = await instance.get<getDiscoverMoviesResponse>(
@@ -43,7 +51,8 @@ export const getDiscoverMovies =  async ({ releaseDataGte, releaseDataLte }: Get
           params: {
             ['release_date.gte']: releaseDataGte,
             ['release_date_lte']: releaseDataLte,
-            region: 'KR'
+            region: 'KR',
+            page: page,
          },
         }
     );
@@ -64,3 +73,118 @@ export const getDiscoverMovies =  async ({ releaseDataGte, releaseDataLte }: Get
         totalResults: response.data.total_results,
     }
 };
+
+interface GetMovieDetailsParams {
+    id: number;
+}
+
+
+interface GetMovieResponse {
+    id: number;
+    genres: {
+      id: number;
+      name: string;
+    };
+    title: string;
+    original_title: string;
+    overview: string | null;
+    poster_path: string | null;
+    release_date: string;
+    runtime: string | null;
+    credits: GetCreditsResponse;
+    videos: GetVideosResponse;
+    release_dates: GetReleaseDatesResponse;
+  }
+  
+  interface GetCreditsResponse {
+    cast: {
+      id: number;
+      known_for_department: string;
+      name: string;
+      profile_path: string | null;
+      character: string;
+    }[];
+    crew: {
+      id: number;
+      known_for_department: string;
+      name: string;
+      profile_path: string | null;
+      job: string;
+    }[];
+  }
+  
+  interface GetVideosResponse {
+    results: {
+      name: string;
+      key: string;
+      site: string;
+      type: string;
+      id: string;
+    }[];
+  }
+  
+  interface GetReleaseDatesResponse {
+    id: number;
+    results: {
+      iso_3166_1: string;
+      release_dates: {
+        certification: string;
+        iso_639_1: string;
+        release_date: string;
+        type: number;
+        note: string;
+      }[];
+    }[];
+  }
+
+
+  export const getMovieDetails = async ({ id }: GetMovieDetailsParams) => {
+    const { data: movie } = await instance.get<GetMovieResponse>(`movie/${id}`, {
+      params: {
+        append_to_response: 'credits,videos,release_dates',
+      },
+    });
+  
+    const releaseDate = (() => {
+      const releaseDateKR = movie.release_dates.results.find(
+        r => r.iso_3166_1 === 'KR',
+      )?.release_dates[0].release_date;
+  
+      if (releaseDateKR != null) {
+        return moment(releaseDateKR).format('YYYY-MM-DD');
+      }
+      return movie.release_date;
+    })();
+  
+    return {
+      id: movie.id,
+      genres: movie.genres,
+      title: movie.title,
+      originalTitle: movie.original_title,
+      overview: movie.overview,
+      posterUrl: getImageUrl(movie.poster_path),
+      releaseDate: releaseDate,
+      runtime: movie.runtime,
+      casts: movie.credits.cast.map(cast => ({
+        id: cast.id,
+        knownForDepartment: cast.known_for_department,
+        name: cast.name,
+        profileUrl: getImageUrl(cast.profile_path),
+        character: cast.character,
+      })),
+      crews: movie.credits.crew.map(crew => ({
+        id: crew.id,
+        knownForDepartment: crew.known_for_department,
+        name: crew.name,
+        profileUrl: getImageUrl(crew.profile_path),
+        job: crew.job,
+      })),
+      videos: movie.videos.results.map(video => ({
+        name: video.name,
+        key: video.key,
+        site: video.site,
+        type: video.type,
+        id: video.id,
+      })),
+    };
+  };
