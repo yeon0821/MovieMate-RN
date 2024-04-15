9a496afdebe0c98f670a34ad518040e8a7dd6242
yeonu <yeon8747@naver.com>
2024-04-15 14:49:51 +0900

    ♻️ :: Screen 코드 수정

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,13 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback } from 'react';
-import { Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Colors from 'open-color';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.black
+    backgroundColor: Colors.black,
   },
   header: {
     height: 48,
@@ -30,53 +38,50 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: Colors.white,
   },
-  body: {
+  content: {
     flex: 1,
   },
-  backButtonIcon: {
-    color: Colors.white,
+  backIcon: {
     fontSize: 20,
+    color: Colors.white,
     marginLeft: 20,
   },
-}); 
+});
 
-interface ScreenProps {
-  title?: string;
+interface ScreenProp {
   children?: React.ReactNode;
+  title?: string;
   headerVisible?: boolean;
 }
 
-const Screen = ({ children, title, headerVisible = true}: ScreenProps) => {
+const Screen = ({ children, title, headerVisible = true }: ScreenProp) => {
   const { goBack, canGoBack } = useNavigation();
   const onPressBackButton = useCallback(() => {
     goBack();
   }, [goBack]);
   return (
     <SafeAreaView style={styles.container}>
-        {
-            Platform.OS === 'ios' ? (
-                <StatusBar barStyle="light-content"/>
-            ) : (
-                <StatusBar barStyle="dark-content"/>
-        )}
-        {headerVisible && (
-      <View style={styles.header}>
-        <View style={styles.left}>
-          {
-          canGoBack() && (
-            <TouchableOpacity onPress={onPressBackButton}>
-                <Icon style={styles.backButtonIcon} name="arrow-back"/>
-            </TouchableOpacity>
-            
-          )}
+      {Platform.OS === 'ios' ? (
+        <StatusBar barStyle="light-content" />
+      ) : (
+        <StatusBar barStyle="dark-content" />
+      )}
+      {headerVisible && (
+        <View style={styles.header}>
+          <View style={styles.left}>
+            {canGoBack() && (
+              <TouchableOpacity onPress={onPressBackButton}>
+                <Icon style={styles.backIcon} name="arrow-back" />
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.center}>
+            <Text style={styles.headerTitle}>{title}</Text>
+          </View>
+          <View style={styles.right} />
         </View>
-        <View style={styles.center}>
-          <Text style={styles.headerTitle}>{title}</Text>
-        </View>
-        <View style={styles.right} />
-      </View>
-        )}
-      <View style={styles.body}>{children}</View>
+      )}
+      <View style={styles.content}>{children}</View>
     </SafeAreaView>
   );
 };
