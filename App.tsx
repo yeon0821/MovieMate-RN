4906cd9d35afd8434b46d81d7c397400615d9603
yeonu <yeon8747@naver.com>
2024-04-15 10:18:41 +0900

    ⚡️ :: DetailScreen 추가

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { type PropsWithChildren } from 'react';
 import { RootStackParmList } from './src/types';
-import MoviesScreen from './src/screens/MoviesScreen';
+import MoviesScreen from './src/screens/movie/MoviesScreen';
 import { QueryClient, QueryClientProvider } from 'react-query'
+import DetailScreen from './src/screens/detail/DetailScreen';
+
 
 const Stack = createNativeStackNavigator<RootStackParmList>();
 
@@ -14,7 +15,8 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
             <NavigationContainer>
                 <Stack.Navigator screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="Movies" component={MoviesScreen} />
+                    <Stack.Screen name="Movies"  component={MoviesScreen} />
+                    <Stack.Screen name="Detail" component={DetailScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
         </QueryClientProvider>
