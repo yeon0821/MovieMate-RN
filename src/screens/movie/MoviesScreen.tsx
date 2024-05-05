2fa31085dee9da1a54079ec540abf9ca9d022a4d
yeonu <yeon8747@naver.com>
2024-05-06 03:36:04 +0900

    ♻️ ::  코드 수정

--- src/screens/movie/MoviesScreen.tsx
+++ src/screens/movie/MoviesScreen.tsx
@@ -1,72 +1,114 @@
-import React from "react";
-import useMovies from "./useMovies";
-import { ActivityIndicator, FlatList, RefreshControl, StyleSheet, View } from "react-native";
-import Movie from "./Movie";
-import Colors from 'open-color'
-import Screen from "../../components/Screen";
-import { useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParmList } from "../../types";
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import mobileAds from 'react-native-google-mobile-ads';
+import Movie from './Movie';
+import useMovies from './useMovies';
+import Colors from 'open-color';
+import Screen from '../../components/Screen';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParmList } from '../../types';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.black,
-    },
-    movieList: {
-        padding: 20,
-    },
-    separator: {
-        height: 10,
-    },
-    lodingContainer: {
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: Colors.black,
+  },
+  movieList: {
+    padding: 20,
+  },
+  separator: {
+    height: 16,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerRightComponent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  alarmButton: {},
+  alarmIcon: {
+    fontSize: 24,
+    color: Colors.white,
+  },
 });
 
 const MoviesScreen = () => {
-    const { movies, isLoading, loadMore, canLoadMore, refresh } = useMovies();
-    const { navigate } =
+  const { movies, isLoading, loadMore, canLoadMore, refresh } = useMovies();
+  const { navigate } =
     useNavigation<NativeStackNavigationProp<RootStackParmList>>();
-    return( 
 
-    <Screen headerVisible = {false}>
+  const [adsInitialized, setAdsInitialized] = useState(false);
+  useEffect(() => {
+    (async () => {
+      await mobileAds().initialize();
+      setAdsInitialized(true);
+    })();
+  }, []);
 
-
-        {isLoading ? < View style={styles.lodingContainer}><ActivityIndicator /></View>:(
-                <FlatList
-                contentContainerStyle={styles.movieList}
-                data={movies}
-                renderItem={({ item: movie }) => (
-                 <Movie 
-                    id={movie.id}
-                    title={movie.title}
-                    originalTitle={movie.originalTitle}
-                    releaseDate={movie.releaseDate}
-                    overview={movie.overview}
-                    posterUrl={movie.posterUrl ?? undefined}
-                    />
-                )}
-                ItemSeparatorComponent={() => <View style={styles.separator} /> }
-                onEndReached={() => {
-                if(canLoadMore){
-                    loadMore();
-                }
-            }}
-            refreshControl={
-              <RefreshControl
-                tintColor={Colors.white} 
-                refreshing={isLoading} 
-                onRefresh={refresh} 
-                /> 
-             }
-            />
-         )
-     }
-    </Screen>
+  const renderRightComponent = useCallback(() => {
+    return (
+      <View style={styles.headerRightComponent}>
+        <TouchableOpacity
+          style={styles.alarmButton}
+          onPress={() => {
+            navigate('Reminders');
+          }}>
+          <Icon name="notifications" style={styles.alarmIcon} />
+        </TouchableOpacity>
+      </View>
     );
+  }, [navigate]);
+
+  return (
+    <Screen renderRightComponent={renderRightComponent}>
+      {isLoading || !adsInitialized ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <FlatList
+          contentContainerStyle={styles.movieList}
+          data={movies}
+          renderItem={({ item: movie }) => (
+            <Movie
+              id={movie.id}
+              title={movie.title}
+              originalTitle={movie.originalTitle}
+              releaseDate={movie.releaseDate}
+              overview={movie.overview}
+              posterUrl={movie.posterUrl ?? undefined}
+            />
+          )}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          onEndReached={() => {
+            if (canLoadMore) {
+              loadMore();
+            }
+          }}
+          refreshControl={
+            <RefreshControl
+              tintColor={Colors.white}
+              refreshing={isLoading}
+              onRefresh={refresh}
+            />
+          }
+        />
+      )}
+    </Screen>
+  );
 };
 
 export default MoviesScreen;
